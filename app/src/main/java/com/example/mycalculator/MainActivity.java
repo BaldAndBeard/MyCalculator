02f21e412b0c4fc8ff9c7eadac94c641c65fc383
BaldAndBeard <64823688+BaldAndBeard@users.noreply.github.com>
2020-05-13 16:36:25 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String expressionValue1 = "";
+/*  private String expressionValue1 = "";
     private String expressionValue2 = "";
     private float value1;
-    private float value2;
+    private float value2;*/
     private String Expression = "";
+    private List<String> myExpression = new ArrayList<>();
+    private List<Float> myNums = new ArrayList<>();
+    private int i = myExpression.size();
     private TextView mshowResult;
 
     @Override
@@ -23,211 +30,197 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Zero(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "0";
+        if (myExpression.size()%2==0) {
+            myExpression.add("0");
             Expression = Expression + "0";
             mshowResult.setText(Expression);
-            value2 = Float.parseFloat(expressionValue2);
         } else {
-            expressionValue1 = expressionValue1 + "0";
+            myExpression.set(i, (myExpression.get(i) + "0"));
             Expression = Expression + "0";
             mshowResult.setText(Expression);
-            value1 = Float.parseFloat(expressionValue1);
         }
     }
 
     public void One(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "1";
+        if (myExpression.size()%2==0) {
+            myExpression.add("1");
             Expression = Expression + "1";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
-
         } else {
-            expressionValue1 = expressionValue1 + "1";
+            myExpression.set(i, (myExpression.get(i).concat("1")));
             Expression = Expression + "1";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
-
         }
     }
 
     public void Two(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "2";
+        if (myExpression.size()%2==0) {
+            myExpression.add("2");
             Expression = Expression + "2";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "2";
+            myExpression.set(i, (myExpression.get(i) + "2"));
             Expression = Expression + "2";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Three(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "3";
+        if (myExpression.size()%2==0) {
+            myExpression.add("3");
             Expression = Expression + "3";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "3";
+            myExpression.set(i, (myExpression.get(i) + "3"));
             Expression = Expression + "3";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Four(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "4";
+        if (myExpression.size()%2==0) {
+            myExpression.add("4");
             Expression = Expression + "4";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "4";
+            myExpression.set(i, (myExpression.get(i) + "4"));
             Expression = Expression + "4";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Five(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "5";
+        if (myExpression.size()%2==0) {
+            myExpression.add("5");
             Expression = Expression + "5";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "5";
+            myExpression.set(i, (myExpression.get(i) + "5"));
             Expression = Expression + "5";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Six(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "6";
+        if (myExpression.size()%2==0) {
+            myExpression.add("6");
             Expression = Expression + "6";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "6";
+            myExpression.set(i, (myExpression.get(i) + "6"));
             Expression = Expression + "6";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Seven(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "7";
+        if (myExpression.size()%2==0) {
+            myExpression.add("7");
             Expression = Expression + "7";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "7";
+            myExpression.set(i, (myExpression.get(i) + "7"));
             Expression = Expression + "7";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Eight(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "8";
+        if (myExpression.size()%2==0) {
+            myExpression.add("8");
             Expression = Expression + "8";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "8";
+            myExpression.set(i, (myExpression.get(i) + "8"));
             Expression = Expression + "8";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Nine(View view) {
-        if (Expression.contains("+")||Expression.contains("-")||Expression.contains("*")||Expression.contains("/")){
-            expressionValue2 = expressionValue2 + "9";
+        if (myExpression.size()%2==0) {
+            myExpression.add("9");
             Expression = Expression + "9";
-            value2 = Float.parseFloat(expressionValue2);
             mshowResult.setText(Expression);
         } else {
-            expressionValue1 = expressionValue1 + "9";
+            myExpression.set(i, (myExpression.get(i) + "9"));
             Expression = Expression + "9";
-            value1 = Float.parseFloat(expressionValue1);
             mshowResult.setText(Expression);
         }
     }
 
     public void Clear(View view) {
-        expressionValue1 = "";
-        expressionValue2 = "";
+        myExpression.clear();
         Expression = "";
         mshowResult.setText(Expression);
     }
 
-
     public void Equals(View view) {
-        if (Expression.contains("+") && Expression.contains(expressionValue1) && Expression.contains(expressionValue2)) {
-            Expression = Float.toString(value1 + value2);
-            mshowResult.setText(Expression);
-            expressionValue1 = Expression;
-            expressionValue2 = "";
-            value1 = Float.parseFloat(expressionValue1);
-        } else if (Expression.contains("-") && Expression.contains(expressionValue1) && Expression.contains(expressionValue2)){
-            Expression = Float.toString(value1 - value2);
-            mshowResult.setText(Expression);
-            expressionValue1 = Expression;
-            expressionValue2 = "";
-            value1 = Float.parseFloat(expressionValue1);
-        } else if (Expression.contains("*") && Expression.contains(expressionValue1) && Expression.contains(expressionValue2)){
-            Expression = Float.toString(value1 * value2);
-            mshowResult.setText(Expression);
-            expressionValue1 = Expression;
-            expressionValue2 = "";
-            value1 = Float.parseFloat(expressionValue1);
-        } else if (Expression.contains("/") && Expression.contains(expressionValue1) && Expression.contains(expressionValue2)) {
-            Expression = Float.toString(value1 / value2);
-            mshowResult.setText(Expression);
-            expressionValue1 = Expression;
-            expressionValue2 = "";
-            value1 = Float.parseFloat(expressionValue1);
+        for (int n = myExpression.size() - 1; n > 0; n--) {
+            switch (myExpression.get(n)) {
+                case "*":
+                    myExpression.set(n + 1, Float.toString(Float.parseFloat(myExpression.get(n - 1)) * Float.parseFloat(myExpression.get(n + 1))));
+                    myExpression.remove(n);
+                    myExpression.remove(n - 1);
+                    continue;
+                case "/":
+                    myExpression.set(n + 1, Float.toString(Float.parseFloat(myExpression.get(n - 1)) / Float.parseFloat(myExpression.get(n + 1))));
+                    myExpression.remove(n);
+                    myExpression.remove(n - 1);
+                    break;
+            }
         }
+        for (int n = myExpression.size() - 1; n > 0; n--) {
+            switch (myExpression.get(n)) {
+                case "+":
+                    myExpression.set(n + 1, Float.toString(Float.parseFloat(myExpression.get(n - 1)) + Float.parseFloat(myExpression.get(n + 1))));
+                    myExpression.remove(n);
+                    myExpression.remove(n-1);
+                    continue;
+                case "-":
+                    myExpression.set(n + 1, Float.toString(Float.parseFloat(myExpression.get(n - 1)) - Float.parseFloat(myExpression.get(n + 1))));
+                    myExpression.remove(n);
+                    myExpression.remove(n-1);
+                    break;
+            }
+
+        }
+        Expression = myExpression.get(0);
+        mshowResult.setText(Expression);
+        myExpression.clear();
+        Expression = "";
     }
 
     public void Plus(View view) {
-        if (!Expression.contains("+")&&!Expression.contains("-")&&!Expression.contains("*")&&!Expression.contains("/")){
+        if (myExpression.size()%2==1) {
+            myExpression.add("+");
             Expression = Expression + "+";
             mshowResult.setText(Expression);
         }
     }
 
     public void Minus(View view) {
-        if (!Expression.contains("+")&&!Expression.contains("-")&&!Expression.contains("*")&&!Expression.contains("/")){
+        if (myExpression.size()%2==1) {
+            myExpression.add("-");
             Expression = Expression + "-";
             mshowResult.setText(Expression);
         }
     }
 
     public void Multiply(View view) {
-        if (!Expression.contains("+")&&!Expression.contains("-")&&!Expression.contains("*")&&!Expression.contains("/")){
+        if (myExpression.size()%2==1) {
+            myExpression.add("*");
             Expression = Expression + "*";
             mshowResult.setText(Expression);
         }
     }
 
     public void Divide(View view) {
-        if (!Expression.contains("+")&&!Expression.contains("-")&&!Expression.contains("*")&&!Expression.contains("/")){
+        if (myExpression.size()%2==1) {
+            myExpression.add("/");
             Expression = Expression + "/";
             mshowResult.setText(Expression);
         }
     }
-
-
 
 }
